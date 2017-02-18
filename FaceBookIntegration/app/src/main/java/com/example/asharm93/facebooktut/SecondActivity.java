902d93e3d96ef7a8c234ff24489cb4f633578294
Sharma <asharm93@LAMU02PG1VMG3QD.uhc.com>
2017-02-18 22:40:46 +0530

    FaceBook Integration

---
+++ FaceBookIntegration/app/src/main/java/com/example/asharm93/facebooktut/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.asharm93.facebooktut;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by asharm93 on 2/17/17.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
